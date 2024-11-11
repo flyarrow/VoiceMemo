39fab499c58759b128badc6dd2492faaddc84696
John Chen <john.cxh@gmail.com>
2024-11-11 10:44:14 +0800

    Initial Commit

---
+++ VoiceMemo/VoiceMemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceMemoApp.swift
+//  VoiceMemo
+//
+//  Created by XIAO HAN CHEN on 2024/11/11.
+//
+
+import SwiftUI
+
+@main
+struct VoiceMemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VoiceMemoTests/VoiceMemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceMemoTests.swift
+//  VoiceMemoTests
+//
+//  Created by XIAO HAN CHEN on 2024/11/11.
+//
+
+import Testing
+@testable import VoiceMemo
+
+struct VoiceMemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
